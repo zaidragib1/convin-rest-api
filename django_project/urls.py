2f737a74de62dc803c91c8331ae7f0c2a8d2497f
MOHD-ZAID-RAGIB <15851710-MOHD-ZAID-RAGIB@users.noreply.replit.com>
2023-05-29 14:31:51 +0000

    Initial commit

---
+++ django_project/urls.py
@@ -0,0 +1,23 @@
+"""django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('v1/calendar/init/', views.google_calendar_init_view, name='google_permission'),
+    path('v1/calendar/redirect/', views.google_calendar_redirect_view, name='google_redirect')
+]
